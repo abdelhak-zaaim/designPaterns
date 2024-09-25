b4ba88f023fa7c03d12e4e42f8fc6d77df013e68
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-09-25 17:34:04 +0100

    add AmericanRestaurant

---
+++ src/main/java/com/design/pattern/bridge/abstraction/AmericanRestaurant.java
@@ -0,0 +1,26 @@
+package com.design.pattern.bridge.abstraction;
+
+import com.design.pattern.bridge.implementation.Pizza;
+
+public class AmericanRestaurant extends Restaurant {
+
+    public AmericanRestaurant(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public void addToppings() {
+        pizza.setToppings("Everything");
+    }
+
+    @Override
+    public void addSauce() {
+        pizza.setSauce("Super Secret Recipe");
+    }
+
+    @Override
+    public void makeCrust() {
+        pizza.setCrust("Thick");
+    }
+
+}
